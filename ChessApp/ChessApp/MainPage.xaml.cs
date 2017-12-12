268731991e89fe347e1cb8ddcb1fa6b8a859cbd6
Mariana <marianacazaresm@gmail.com>
2017-12-12 11:59:42 -0800

    grid

--- ChessApp/ChessApp/MainPage.xaml.cs
+++ ChessApp/ChessApp/MainPage.xaml.cs
@@ -29,18 +29,62 @@ namespace ChessApp
         {
             this.InitializeComponent();
             ConfigureHub();
+            CreateGrid();
         }
 
+        private void CreateGrid()
+        {
+            int ButtonWidth = 50;
+            int ButtonHeight = 50;
+
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    Button tmpButton = new Button
+                    {
+                        Width = ButtonWidth,
+                        Height = ButtonHeight
+                    };
+
+                    if(x % 2 == 0 && y % 2 == 0)
+                    {
+                        tmpButton.Foreground = 
+                    }
+
+                    Grid.SetColumn(tmpButton, y);
+                    Grid.SetRow(tmpButton, x);
+                    this.board.Children.Add(tmpButton);
+                }
+            }
+
+            //Button button = new Button
+            //{
+            //    Height = ButtonHeight,
+            //    Width = ButtonWidth
+            //};
+
+            //for(int x = 1; x <= 64; x++)
+            //{
+            //    board.Children.Add(button);
+            //}
+            
+        }
+
+        //Hello world button click
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             SendMessage(TextBoxMessage.Text.ToString());
         }
 
+        //Send message to signal R service
         private void SendMessage(string msg)
         {
             _hub.Invoke("SendAction", msg);
         }
 
+
+        //Signal R connection
         public void ConfigureHub()
         {
             string url = @"http://xpesdcompany-001-site3.ctempurl.com/apis/ChessNotificationsService/signalr/hubs";
@@ -52,6 +96,8 @@ namespace ChessApp
                 var x = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
                 {
                     lstMessages.Items.Add(result);
+                    lstMessages.ScrollIntoView(lstMessages.Items[lstMessages.Items.Count - 1]);
+                    
                 });
             });
             connection.Start().Wait();
